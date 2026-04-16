957d0071b5b28d25d051370a9b142e137f61504b
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2026-04-16 22:23:28 +0200

    Encode passwords before saving  user to database

--- src/main/java/com/example/booksapi/infrastructure/userregister/UserRegisterController.java
+++ src/main/java/com/example/booksapi/infrastructure/userregister/UserRegisterController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,11 +21,17 @@ import org.springframework.web.bind.annotation.RestController;
 class UserRegisterController {
 
     private final UserRegisterFacade userRegisterFacade;
+    private final PasswordEncoder passwordEncoder;
 
     @PostMapping
     ResponseEntity<RegisterResponseDto> register(@RequestBody @Valid final RegisterRequestDto requestDto) {
         log.info("Received HTTP request to register user: {}", requestDto);
-        RegisterResponseDto responseDto = userRegisterFacade.register(requestDto);
+        final String username = requestDto.username();
+        final String password = passwordEncoder.encode(requestDto.password());
+        RegisterResponseDto responseDto = userRegisterFacade.register(RegisterRequestDto.builder()
+                .username(username)
+                .password(password)
+                .build());
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
     }
 
